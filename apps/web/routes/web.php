52e596934a294fc8fd7ca31c4e095edb536e8440
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-08-12 20:55:22 +0200

    feat: remove docs and add routes for legal pages

--- apps/web/routes/web.php
+++ apps/web/routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
-use Pfy\Web\Http\Controllers\Inertia\CommandController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Pfy\Web\Http\Controllers\Inertia\CommandController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
 });
+
+Route::get('privacy-policy', function () {
+    return Inertia::render('PrivacyPolicy');
+})->name('privacy-policy');
+
+Route::get('terms-of-service', function () {
+    return Inertia::render('TermsOfService');
+})->name('terms-of-service');
 
 Route::middleware([
     'auth:sanctum',
@@ -20,10 +28,4 @@ Route::middleware([
     Route::get('commands/{command}/edit', [CommandController::class, 'edit'])->name('command.edit');
     Route::put('commands/{command}', [CommandController::class, 'update'])->name('command.update');
     Route::delete('commands/{command}', [CommandController::class, 'destroy'])->name('command.destroy');
-
-    Route::get('docs', function () {
-        return Inertia::render('Docs', [
-            'content' => file_get_contents(base_path('README.md')),
-        ]);
-    })->name('docs.index');
 });
