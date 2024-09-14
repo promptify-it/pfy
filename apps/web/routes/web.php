d83cfdea05bdcbe65588137e32a8545a15b8ab2a
Daniele Tulone <35412801+danieletulone@users.noreply.github.com>
2024-09-14 09:38:39 +0200

    feat: add clone command

--- apps/web/routes/web.php
+++ apps/web/routes/web.php
@@ -8,13 +8,17 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('welcome');
 
+Route::get('/marketplace', function () {
+    return Inertia::render('Marketplace');
+})->name('marketplace');
+
 Route::get('privacy-policy', function () {
     return Inertia::render('PrivacyPolicy');
-})->name('privacy-policy');
+})->name('policy.show');
 
 Route::get('terms-of-service', function () {
     return Inertia::render('TermsOfService');
-})->name('terms-of-service');
+})->name('terms.show');
 
 Route::middleware([
     'auth:sanctum',
@@ -25,6 +29,7 @@ Route::middleware([
     Route::get('commands/create', [CommandController::class, 'create'])->name('command.create');
     Route::post('commands/create', [CommandController::class, 'store'])->name('command.store');
     Route::get('commands/{command}', [CommandController::class, 'show'])->name('command.show');
+    Route::post('commands/{command}/clone', [CommandController::class, 'clone'])->name('command.clone');
     Route::get('commands/{command}/edit', [CommandController::class, 'edit'])->name('command.edit');
     Route::put('commands/{command}', [CommandController::class, 'update'])->name('command.update');
     Route::delete('commands/{command}', [CommandController::class, 'destroy'])->name('command.destroy');
